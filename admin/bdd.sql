9ffaf9de3b7429089b5defbc365ad91a2137e30a
Lily <112404773+Belywen@users.noreply.github.com>
2025-01-26 16:49:54 +0100

    maj bdd

--- admin/bdd.sql
+++ admin/bdd.sql
@@ -22,48 +22,49 @@ USE `sfsessionlily`;
 -- Listage de la structure de table sfsessionlily. category
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `libelle_category` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `libelle_category` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.category : ~0 rows (environ)
-DELETE FROM `category`;
 
 -- Listage de la structure de table sfsessionlily. doctrine_migration_versions
 CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
-  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `version` varchar(191) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `executed_at` datetime DEFAULT NULL,
   `execution_time` int DEFAULT NULL,
   PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- Listage des données de la table sfsessionlily.doctrine_migration_versions : ~1 rows (environ)
-DELETE FROM `doctrine_migration_versions`;
-INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+-- Listage des données de la table sfsessionlily.doctrine_migration_versions : ~0 rows (environ)
+REPLACE INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
 	('DoctrineMigrations\\Version20250120130936', '2025-01-20 13:10:08', 589);
 
 -- Listage de la structure de table sfsessionlily. intern
 CREATE TABLE IF NOT EXISTS `intern` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name_intern` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date_birth` datetime NOT NULL,
-  `sex` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `city` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name_intern` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_birth` date NOT NULL,
+  `sex` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table sfsessionlily.intern : ~0 rows (environ)
-DELETE FROM `intern`;
+-- Listage des données de la table sfsessionlily.intern : ~3 rows (environ)
+REPLACE INTO `intern` (`id`, `name_intern`, `firstname`, `date_birth`, `sex`, `city`, `email`, `phone`) VALUES
+	(1, 'Renau', 'Laury', '1988-03-14', 'F', 'Chalons', 'lily@gmail.fr', '0707070707'),
+	(2, 'Vautier', 'Elise', '2025-01-26', 'F', 'Strasbourg', 'elise@gmail.fr', '0707070707'),
+	(3, 'Ruffo', 'Yofer', '1985-10-25', 'M', 'Strasbourg', 'yofer@gmail.fr', '0707070700707');
 
 -- Listage de la structure de table sfsessionlily. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
@@ -74,20 +75,18 @@ CREATE TABLE IF NOT EXISTS `messenger_messages` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.messenger_messages : ~0 rows (environ)
-DELETE FROM `messenger_messages`;
 
 -- Listage de la structure de table sfsessionlily. module
 CREATE TABLE IF NOT EXISTS `module` (
   `id` int NOT NULL AUTO_INCREMENT,
   `category_id` int DEFAULT NULL,
-  `libelle_module` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `libelle_module` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_C24262812469DE2` (`category_id`),
   CONSTRAINT `FK_C24262812469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.module : ~0 rows (environ)
-DELETE FROM `module`;
 
 -- Listage de la structure de table sfsessionlily. program
 CREATE TABLE IF NOT EXISTS `program` (
@@ -103,14 +102,13 @@ CREATE TABLE IF NOT EXISTS `program` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.program : ~0 rows (environ)
-DELETE FROM `program`;
 
 -- Listage de la structure de table sfsessionlily. session
 CREATE TABLE IF NOT EXISTS `session` (
   `id` int NOT NULL AUTO_INCREMENT,
   `training_id` int DEFAULT NULL,
   `trainer_id` int DEFAULT NULL,
-  `libelle_session` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `libelle_session` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `nb_place` int NOT NULL,
   `date_begin` datetime NOT NULL,
   `date_end` datetime NOT NULL,
@@ -122,7 +120,6 @@ CREATE TABLE IF NOT EXISTS `session` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.session : ~0 rows (environ)
-DELETE FROM `session`;
 
 -- Listage de la structure de table sfsessionlily. session_intern
 CREATE TABLE IF NOT EXISTS `session_intern` (
@@ -136,45 +133,42 @@ CREATE TABLE IF NOT EXISTS `session_intern` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.session_intern : ~0 rows (environ)
-DELETE FROM `session_intern`;
 
 -- Listage de la structure de table sfsessionlily. trainer
 CREATE TABLE IF NOT EXISTS `trainer` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name_trainer` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name_trainer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.trainer : ~0 rows (environ)
-DELETE FROM `trainer`;
 
 -- Listage de la structure de table sfsessionlily. training
 CREATE TABLE IF NOT EXISTS `training` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `libelle_training` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `libelle_training` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.training : ~0 rows (environ)
-DELETE FROM `training`;
 
 -- Listage de la structure de table sfsessionlily. user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` json NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table sfsessionlily.user : ~1 rows (environ)
-DELETE FROM `user`;
-INSERT INTO `user` (`id`, `nickname`, `email`, `roles`, `password`) VALUES
-	(2, 'Lisou', 'lisou@gmail.com', '[]', '1234');
+REPLACE INTO `user` (`id`, `nickname`, `email`, `roles`, `password`) VALUES
+	(3, NULL, 'root@gmail.com', '[]', '$2y$13$nyas23qpFwN5jxR1W1GNf.2ranC.zdWdyxrKdaLv/.CiSRAiJN7MO'),
+	(4, NULL, 'lisou@gmail.com', '[]', '$2y$13$d8LaFUnAGst.zo7Jw.nw6.oTUtUaKZx8JFHTMThTNkwsdGAK7d5pG');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
