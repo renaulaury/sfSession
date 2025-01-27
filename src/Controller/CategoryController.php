2f7dc6d16836ca3b79c672ac1a9062b967704187
Laury <112404773+Belywen@users.noreply.github.com>
2025-01-27 10:22:08 +0100

    form ajout de category ok

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,24 +2,47 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class CategoryController extends AbstractController
 {
+    //Afficher la liste des catégories
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(CategoryRepository $catRepo ): Response
     {
+
+        $categories = $catRepo->findAll();
+
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories
         ]);
     }
 
+    // Affiche le formulaire d'ajout
     #[Route('/category/addCat', name: 'app_addCat')]
-    public function addCat(): Response
+    public function addCat(Request $request, EntityManagerInterface $entityManager): Response
     {
+
+        $category = new Category;
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $intern = $form->getData();
+            $entityManager->persist($intern);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_category');
+        }
+
         return $this->render('category/addCat.html.twig', [
+            'formCategory' => $form,
         ]);
     }
 
--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ModuleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class ModuleController extends AbstractController
 {
     #[Route('/module', name: 'app_module')]
-    public function index(): Response
+    public function index(ModuleRepository $moduleRepo): Response
     {
+        $modules = $moduleRepo->findAll();
+        
         return $this->render('module/index.html.twig', [
-            'controller_name' => 'ModuleController',
+            'modules' => $modules,
         ]);
     }
 
